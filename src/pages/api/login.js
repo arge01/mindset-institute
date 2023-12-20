3a002133700c40b117897ad2fc22484452e04f7c
KRON-ARGEVENCI <arif.gevenci@kron.com.tr>
2023-12-20 20:25:19 +0300

    network problem

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -18,32 +18,40 @@ async function LoginRequest(req, res) {
       const network = await os.networkInterfaces();
 
       if (token === `Bearer ${backEndToken}`) {
-        fs.appendFile(
-          'ip.txt',
-          `page: ${new Date()} - ${local?.ip ? local?.ip : false} / ${network?.['Wi-Fi']?.[1]?.cidr} \n`
-        );
-        res.status(201).json({
-          isSuccess: true,
-          login: true,
-          user: {
-            id: 15,
-            username: 'kminchelle',
-            email: 'kminchelle@qq.com',
-            firstName: 'Jeanne',
-            lastName: 'Halvorson',
-            gender: 'female',
-            image: 'https://robohash.org/autquiaut.png',
-            token: backEndToken,
-          },
-          token: backEndToken || token,
-          ipV4: requestIp.address() || false,
-          socketIp:
-            req.headers['x-forwarded-for']?.split(',').shift() ||
-            req.socket?.remoteAddress ||
-            false,
-          network,
-          getApi: local?.ip ? local : false,
-        });
+        if (local?.ip) {
+          fs.appendFile(
+            'ip.txt',
+            `page: ${new Date()} - ${local?.ip ? local?.ip : false} / ${
+              network?.['Wi-Fi']?.[1]?.cidr
+            } \n`
+          );
+          res.status(201).json({
+            isSuccess: true,
+            login: true,
+            user: {
+              id: 15,
+              username: 'kminchelle',
+              email: 'kminchelle@qq.com',
+              firstName: 'Jeanne',
+              lastName: 'Halvorson',
+              gender: 'female',
+              image: 'https://robohash.org/autquiaut.png',
+              token: backEndToken,
+            },
+            token: backEndToken || token,
+            ipV4: requestIp.address() || false,
+            socketIp:
+              req.headers['x-forwarded-for']?.split(',').shift() ||
+              req.socket?.remoteAddress ||
+              false,
+            network,
+            getApi: local?.ip ? local : false,
+          });
+        } else {
+          res
+            .status(403)
+            .json({ isSuccess: true, data: 'Access Permission Problem!' });
+        }
       } else {
         res.status(500).json({
           isSuccess: true,
@@ -69,11 +77,13 @@ async function LoginRequest(req, res) {
         const local = await getIp.json();
         const network = await os.networkInterfaces();
 
-        if (network?.['Wi-Fi']?.[1]?.cidr) {
+        if (local?.ip) {
           if (data?.token) {
             fs.appendFile(
               'ip.txt',
-              `login: ${new Date()} - ${local?.ip ? local?.ip : false} / ${network?.['Wi-Fi']?.[1]?.cidr} \n`
+              `login: ${new Date()} - ${local?.ip ? local?.ip : false} / ${
+                network?.['Wi-Fi']?.[1]?.cidr
+              } \n`
             );
 
             res.status(201).json({
